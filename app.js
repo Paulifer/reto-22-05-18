52a3a97e5576d40b54727d01e4c80f67b0fdd783
valeria <valeria.penaloza5@gmail.com>
2018-05-22 20:02:02 -0400

    agragando info, imagenes y css

---
+++ app.js
@@ -0,0 +1,12 @@
+function nameUser(){
+	var user = document.getElementById("name").value;
+	user = document.getElementById("hi").innerHTML = "Hola " + user;
+}
+
+function ysblf(){
+	document.getElementById("infoBetty").style.display = "block";
+}
+
+function close(){
+	document.getElementById("infoBetty").style.display = "none";
+}
